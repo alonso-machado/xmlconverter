0b63875fa17daf04bf080732abade05c1c75daa4
Alonso Machado <84375734+alonso-machado@users.noreply.github.com>
2024-02-12 12:39:09 -0400

    Mutation Test PITEST

--- target/generated-sources/annotations/com/alonso/xmlconverter/mapper/CustomerMapperImpl.java
+++ target/generated-sources/annotations/com/alonso/xmlconverter/mapper/CustomerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-29T14:44:23-0400",
+    date = "2024-02-12T11:42:58-0400",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 @Component
--- target/generated-sources/annotations/com/alonso/xmlconverter/mapper/MovimentMapperImpl.java
+++ target/generated-sources/annotations/com/alonso/xmlconverter/mapper/MovimentMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-29T14:44:22-0400",
+    date = "2024-02-12T11:42:58-0400",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.5 (Eclipse Adoptium)"
 )
 @Component
